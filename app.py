85a28043f857ada616ce032c672d1e221e648151
Kethan Vegunta <kethan@vegunta.com>
2021-03-16 20:55:45 -0700

    add function to convert degrees from joystick into robot values

--- app.py
+++ app.py
@@ -17,8 +17,23 @@ right_backward = GPIO.PWM(26, 50)
 left_backward = GPIO.PWM(19, 50)
 left_forward = GPIO.PWM(13, 50)
 
+speed=50
+
 motors = [right_forward, right_backward, left_backward, left_forward]
 for motor in motors: motor.stop()
+
+# assumes theta in degrees and r = 0 to 100 %
+# returns a tuple of percentages: (left_thrust, right_thrust)
+def throttle_angle_to_thrust(theta, r=speed):
+    theta = ((theta + 180) % 360) - 180  # normalize value to [-180, 180)
+    r = min(max(0, r), 100)              # normalize value to [0, 100]
+    v_a = r * (45 - theta % 90) / 45          # falloff of main motor
+    v_b = min(100, 2 * r + v_a, 2 * r - v_a)  # compensation of other motor
+    if theta < -90: return -v_b, -v_a
+    if theta < 0:   return -v_a, v_b
+    if theta < 90:  return v_b, v_a
+    return v_a, -v_b
+
 
 @app.route('/')
 def index():
@@ -46,22 +61,26 @@ def move_robot():
     if request.json["angle"] != "still":
         angle = request.json["angle"]
         print(angle)
-        if 0 <= angle <= 90:
-            percent_right = ((90-angle)/90)*100
-            percent_forward = (angle/90)*100
-            print("forward, right")
-        elif 90 < angle <= 180:
-            print("forward, left")
-        elif -179 <= angle <= -90:
-            percent_left = int(round(((abs(angle)-90)/90)*100, 0))
-            percent_back = int(round((90-percent_left/90)*100, 0))
-            # right_backward.start(percent_back)
-            # left_backward.start(percent_left)
-            print("backward, left", percent_left, percent_back)
-        elif -90 < angle <= -1:
-            percent_right = 0-(0-angle)
-            percent_back = -90-(-90-angle)
-            print("backward, right", percent_right, percent_back)
+        final_angle = None
+        # throttle_angle_to_thrust(final_angle)
+        # if 0 <= angle <= 90:
+        #     percent_right = ((90-angle)/90)*100
+        #     percent_forward = (angle/90)*100
+        #     right_forward.start(percent_forward-percent_right)
+        #     left_forward.start(percent_forward)
+        #     print("forward, right", percent_right, percent_forward)
+        # elif 90 < angle <= 180:
+        #     print("forward, left")
+        # elif -179 <= angle <= -90:
+        #     percent_left = int(round(((abs(angle)-90)/90)*100, 0))
+        #     percent_back = int(round((90-percent_left/90)*100, 0))
+        #     # right_backward.start(percent_back)
+        #     # left_backward.start(percent_left)
+        #     print("backward, left", percent_left, percent_back)
+        # elif -90 < angle <= -1:
+        #     percent_right = 0-(0-angle)
+        #     percent_back = -90-(-90-angle)
+        #     print("backward, right", percent_right, percent_back)
     else:
         for motor in motors: motor.stop()
     return "Good"
